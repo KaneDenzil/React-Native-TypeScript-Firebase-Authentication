37d6d7402e75b54ad92611768aaade550983257c
Kane Bernard <kaneon95@gmail.com>
2022-03-29 11:33:29 -0400

    Modified App.tsx aded comment

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import './src/constants/firebase'
 import React from 'react';
 import MainNav from './src/navigation/mainNav';
-
+//asdjbnaskdhjbaskjhdbaksjhbdkasbdkasbdkasb
  export default function App() {
 
    return (
